9dc211b5e39f4810a0cdc18e0718b504896785c2
thisGuyRon <42878005+thisGuyRon@users.noreply.github.com>
2018-12-07 10:37:33 -0500

    updated for correctness

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,6 +7,10 @@ monthCount=0
 Total=0
 incValue=0
 decValue=0
+avgTotal=0
+prevAmt=0
+currAmt=0
+initPass=1
 #CSV Path to read, home path
 csvpath = os.path.join("budget_data.csv")
 #Text file to write, home path
@@ -19,34 +23,54 @@ with open(csvpath, "r") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
 
-
+    
     # Read each row of data after the header
     for row in csvreader:
-        #Running counter of months and for final average
-        monthCount=monthCount + 1
-        #Running total 
-        Total=Total+int(row[1])
-        #Establish the largest increase value and dates
-        if int(row[1]) > incValue:
-            incValue=int(row[1])
-            incDate=row[0]
-        #Establish the largest decrease and dates
-        if int(row[1]) < decValue:
-            decValue=int(row[1])
-            decDate=row[0]
+        # Gather first record without adding to running average change total
+        if initPass==1:
+            #ignore this code after first pass through csv reader
+            initPass=2
+            prevAmt=int(row[1])
+            monthCount=monthCount + 1
+            #Running total 
+            Total=Total+int(row[1])
+            if int(row[1]) > incValue:
+                incValue=int(row[1])
+                incDate=row[0]
+            #Establish the largest decrease and dates
+            if int(row[1]) < decValue:
+                decValue=int(row[1])
+                decDate=row[0]     
+        else:
+            #Running counter of months and for final average
+            monthCount=monthCount + 1
+            currAmt=int(row[1])
+            avgTotal=avgTotal+currAmt-prevAmt
+            prevAmt=currAmt
+            #Running total 
+            Total=Total+int(row[1])
+            #Establish the largest increase value and dates
+            if int(row[1]) > incValue:
+                incValue=int(row[1])
+                incDate=row[0]
+            #Establish the largest decrease and dates
+            if int(row[1]) < decValue:
+                decValue=int(row[1])
+                decDate=row[0]
 
 #error checking for divisible by 0
 if monthCount==0:
     average=0
 else:
-    average=Total/monthCount
+    average=avgTotal/(monthCount-1)
+
 
 #Output to terminal
 print("Financial Analysis")
 print("---------------------------")
 print("Total Months: " + str(monthCount))
 print("Total: " + str(Total))
-print("Average Change: " + str(average))
+print("Average Change: " + str("%.2f" % average))
 print("Greatest Increase in Profits: " + incDate + " " + str(incValue))
 print("Greatest Decrease in Profits: " + decDate + " " + str(decValue))
 
@@ -58,7 +82,7 @@ file.write("Financial Analysis\n")
 file.write("---------------------------\n")
 file.write("Total Months: " + "(" + str(monthCount) + ")" + "\n")
 file.write("Total: " + "(" + str(Total) + ")" + "\n")
-file.write("Average Change: " + "(" + str(average) + ")" + "\n")
+file.write("Average Change: " + "(" + str("%.2f" % average) + ")" + "\n")
 file.write("Greatest Increase in Profits: " + incDate + " " + "(" + str(incValue) + ")" + "\n")
 file.write("Greatest Decrease in Profits: " + decDate + " " + "(" + str(decValue) + ")"+ "\n")
 
