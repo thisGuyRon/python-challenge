e3aca02bc7647dffe9ecaa959de8ef93dc8db4e9
thisGuyRon <42878005+thisGuyRon@users.noreply.github.com>
2019-03-22 12:54:39 -0400

    refined code and output

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,10 +2,7 @@ import os
 import csv
 #variable instantiation
 Total=0
-KhanTotal=0
-CorreyTotal=0
-LiTotal=0
-OTooleyTotal=0  
+CandCount={}
 #csv path reader, home directory
 csvpath = os.path.join("election_data.csv")
 #text file writer, home directory
@@ -23,60 +20,20 @@ with open(csvpath, "r") as csvfile:
     for row in csvreader:
         #Running total votes counter
         Total=Total+1
-        #running khan vote counter
-        if row[2]=="Khan":
-            KhanTotal=KhanTotal+1
-        #running correy vote counter
-        elif row[2]=="Correy":
-            CorreyTotal=CorreyTotal+1
-        #running Li vote Counter
-        elif row[2]=="Li":
-            LiTotal=LiTotal+1
-        #Running O'Tooley Vote Counter
-        elif row[2]=="O'Tooley":
-            OTooleyTotal=OTooleyTotal+1
-
-#logic to calculate the winner
-if KhanTotal>CorreyTotal:
-    if KhanTotal>LiTotal:
-        if KhanTotal>OTooleyTotal:
-            winner="Khan"
+        if row[2] in CandCount:
+            CandCount[row[2]]+=1
         else:
-            winner="O'Tooley"
-    else:
-        if LiTotal>OTooleyTotal:
-            winner="Li"
-        else:
-            winner="O'Tooley"
-else:
-    if CorreyTotal>LiTotal:
-        if CorreyTotal>OTooleyTotal:
-            winner="Correy"
-        else:
-            Winner="O'Tooley"
-    else:
-        if LiTotal>OTooleyTotal:
-                winner="Li"
-        else:
-                winner="O'Tooley"
-
-#Calculate the % of each candidate
-KhanPercent= (KhanTotal * 100)/Total
-CorreyPercent=(CorreyTotal * 100)/Total
-LiPercent=(LiTotal* 100)/Total
-OTooleyPercent=(OTooleyTotal * 100)/Total
+            CandCount[row[2]] = 1 
 
 #Results printed to terminal
 print("Election Results")
 print("-----------------------------")
 print("Total Votes: " + str(Total))
 print("-----------------------------")
-print("Khan: " + str("%.4f" % KhanPercent) + "% (" + str(KhanTotal) + ")")
-print("Correy: " + str("%.4f" %CorreyPercent) + "% (" + str(CorreyTotal) + ")")
-print("Li: " + str("%.4f" %LiPercent) + "% (" + str(LiTotal) + ")")
-print("O'Tooley: " + str("%.4f" %OTooleyPercent) + "% (" + str(OTooleyTotal) + ")")
+for key, value in CandCount.items():
+    print(key + ": " + "%.4f" %(CandCount.get(key) * 100/Total) + "% (" + str(value) + ")")
 print("-----------------------------")
-print("Winner: " + winner)
+print("Winner: " + max(CandCount, key=CandCount.get))
 print("-----------------------------")
 
 #write file details
@@ -86,10 +43,8 @@ file.write("Election Results\n")
 file.write("-----------------------------\n")
 file.write("Total Votes: " + str(Total) + "\n")
 file.write("-----------------------------\n")
-file.write("Khan: " + str("%.4f" % KhanPercent) + " (" + str(KhanTotal) + ")\n")
-file.write("Correy: " + str("%.4f" % CorreyPercent) + " (" + str(CorreyTotal) + ")\n")
-file.write("Li: " + str("%.4f" % LiPercent) + " (" + str(LiTotal) + ")\n")
-file.write("O'Tooley: " + str("%.4f" % OTooleyPercent) + " (" + str(OTooleyTotal) + ")\n")
+for key, value in CandCount.items():
+    file.write(key + ": " + "%.4f" %(CandCount.get(key) * 100/Total) + "% (" + str(value) + ")\n")
 file.write("-----------------------------\n")
-file.write("Winner: " + winner + "\n")
+file.write("Winner: " + max(CandCount, key=CandCount.get) + "\n")
 file.write("-----------------------------\n")
